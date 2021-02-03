42a9df195a5259f1b98aa8723cac6061af56da15
Juan Carrera <car20593@uvg.edu.gt>
2021-02-03 17:27:01 -0600

    "Arreglo para aceptar divisiones entre 0 y espacios"

--- Test/CalculadoraJACTest.java
+++ Test/CalculadoraJACTest.java
@@ -7,8 +7,8 @@ public class CalculadoraJACTest {
     @Test
     public void calculo() {
         CalculadoraJAC jc = new CalculadoraJAC();
-        String st = jc.calculo("52*7*7+52*7*5*2*+");
-        assertEquals("777",st);
+        String st = jc.calculo("1 2 3 / 4 * + 5 - 6 7 *+");
+        assertEquals("38",st);
 
         st = jc.calculo("5252*7**");
         assertEquals("Operacion No Valida",st);
--- src/CalculadoraJAC.java
+++ src/CalculadoraJAC.java
@@ -14,33 +14,47 @@ public class CalculadoraJAC implements CalculadoraGeneral{
         for (int i = 0; i < texto.length(); i++) {
             char inds = texto.charAt(i);
 
-            if(Character.isDigit(inds)){
-                vec.push(Character.getNumericValue(inds));
+            if(Character.isWhitespace(inds)){ //no espacios en blanco
 
+            }
+
+            else if(Character.isDigit(inds)){
+                vec.push(Character.getNumericValue(inds));
             }
             else {
                 if (vec.size() > 1) {
+                    try{
+                        int n1 = vec.pop();
+                        int n2 = vec.pop();
 
-                    int n1 = vec.pop();
-                    int n2 = vec.pop();
+                        switch (inds) {
+                            case '+' -> {
+                                vec.push(n1 + n2);
+                            }
+                            case '-' -> {
+                                vec.push(n2 - n1);
+                            }
+                            case '*' -> {
+                                vec.push(n1 * n2);
+                            }
+                            case '/' -> {
+                                vec.push(n2 / n1);
+                            }
+                        }
+                    } catch (ArithmeticException e){
+                        if(vec.size() > 1){
+                            for (int j = 0; j <= vec.size(); j++) {
+                                vec.pop();
+                            }
+                        }
 
-                    switch (inds) {
-                        case '+' -> {
-                            vec.push(n1 + n2);
-                        }
-                        case '-' -> {
-                            vec.push(n2 - n1);
-                        }
-                        case '*' -> {
-                            vec.push(n1 * n2);
-                        }
-                        case '/' -> {
-                            vec.push(n2 / n1);
-                        }
+                        return "No divisible entre 0";
                     }
                 }
                 else {
-                    vec.pop();
+                    for (int j = 0; j <= vec.size(); j++) {
+                        vec.pop();
+                    }
                     return "Operacion No Valida";
                 }
             }
@@ -49,9 +63,12 @@ public class CalculadoraJAC implements CalculadoraGeneral{
         if(vec.size() == 1){
             return vec.pop().toString();
         }
-        else{
+        else if (vec.size() > 0){
+            for (int j = 0; j <= vec.size(); j++) {
+                vec.pop();
+            }
             return "Operacion No Valida";
         }
-
+        return "Operacion no valida";
     }
 }
